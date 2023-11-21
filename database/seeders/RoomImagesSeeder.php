fcfa717bfd978674835b172a8bdd0ffe0f5cc29c
Yusup Hidayat <yusufxyz114@gmail.com>
2023-11-21 21:46:41 +0700

    seed

--- database/seeders/RoomImagesSeeder.php
+++ database/seeders/RoomImagesSeeder.php
@@ -14,6 +14,16 @@ class RoomImagesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('rooms')->insert([
+        	'name' => 'GEDUNG MERAH PUTIH',
+        	'description' => 'Gedung merah putih memiliki : lorem ipsum',
+        	'thumbnail' => 'room_images/1699498070_sosialiasasi.jpg',
+        	'capacity' => 50,
+        	'location' => 'Lantai 1',
+        	'availability' => 1,
+        	'created_at' => '2023-11-09 02:47:50',
+        	'updated_at' => '2023-11-09 02:47:50',
+        	'ownership' => 'BAAK'
+        ]);
     }
 }
--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoomSeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class RoomSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // insert data ke table rooms
+        DB::table('rooms')->insert([
+        	'name' => 'GEDUNG MERAH PUTIH',
+        	'description' => 'Gedung merah putih memiliki : lorem ipsum',
+        	'thumbnail' => 'room_images/1699498070_sosialiasasi.jpg',
+        	'capacity' => 50,
+        	'location' => 'Lantai 1',
+        	'availability' => 1,
+        	'created_at' => '2023-11-09 02:47:50',
+        	'updated_at' => '2023-11-09 02:47:50',
+        	'ownership' => 'BAAK'
+        ]);
     }
 }
