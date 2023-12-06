35e9cbeb18c37073ee6680657d765ecfcb925572
Yusup Hidayat <yusufxyz114@gmail.com>
2023-12-06 18:11:33 +0700

    bg image calendar

--- resources/views/content/calendar.blade.php
+++ resources/views/content/calendar.blade.php
@@ -6,7 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
-    <title>Peminjaman Ruangan</title>
+    <title>Data Peminjaman | SIPERU - Aplikasi Peminjaman Ruangan </title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/upr.png') }}" />
+    <style>
+body {
+  background-image: url('{{ asset('assets/img/backgrounds/logo.png') }}');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: 50% 100%;
+}
+</style>
 </head>
 <body>
     <div class="container">
